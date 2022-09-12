2fff276ccfb8e437c08a3ec22fac10aa0e29ae13
Philip Nwokedi <philippos764@gmail.com>
2022-09-12 07:07:46 -0700

    Initial Heroku Push

---
+++ resources/views/components/section/hero.blade.php
@@ -0,0 +1,27 @@
+<section id="hero" class="hero">
+    <div class="container">
+        <div class="row text-white hero--centered">
+            <div class="col-12">
+                <h1 class="display-2">
+                    <span class="display-2--intro">
+                        Nigeria's Biggest 
+                        <span style="display: inline-flex">Y<span class="avatar-3 mt-2"><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
+                        Gathering</span>
+                    <span class="display-2--description">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Cupiditate nam cum, nemo recusandae, suscipit, non explicabo vero sit 
+                        nesciunt labore animi blanditiis fugiat numquam pariatur.
+                    </span>
+                </h1>
+                {{-- <span class="avatar avatar-1"><img src="/images/avatars/avatar-1.png" alt="memoji 1"></span>
+                <span class="avatar avatar-2"><img src="/images/avatars/avatar-2.png" alt="memoji 2"></span>
+                
+                <span class="avatar avatar-4"><img src="/images/avatars/avatar-4.png" alt="memoji 4"></span>
+                <span class="avatar avatar-5"><img src="/images/avatars/avatar-5.png" alt="memoji 5"></span>
+                <span class="avatar avatar-6"><img src="/images/avatars/avatar-6.png" alt="memoji 6"></span>
+                <span class="avatar avatar-7"><img src="/images/avatars/avatar-7.png" alt="memoji 7"></span>
+                <span class="avatar avatar-8"><img src="/images/avatars/avatar-8.png" alt="memoji 8"></span> --}}
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout.main>
+    <x-section.hero />
+</x-layout.main>
