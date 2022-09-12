83948a0cd1d16b16761f560f83689fa70fad205f
Philip Nwokedi <philippos764@gmail.com>
2022-09-12 12:18:18 -0700

    feat: implement image as letter O

--- resources/views/components/section/hero.blade.php
+++ resources/views/components/section/hero.blade.php
@@ -5,7 +5,7 @@
                 <h1 class="display-2">
                     <span class="display-2--intro">
                         Nigeria's Biggest 
-                        <span style="display: inline-flex">Y<span class="avatar-3 mt-2"><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
+                        <span class="avatar-3-wrapper">Y<span class=""><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
                         Gathering</span>
                     <span class="display-2--description">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
@@ -13,15 +13,17 @@
                         nesciunt labore animi blanditiis fugiat numquam pariatur.
                     </span>
                 </h1>
-                {{-- <span class="avatar avatar-1"><img src="/images/avatars/avatar-1.png" alt="memoji 1"></span>
+                {{-- <span class="avatar-3-wrapper">Y<span><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
+                 --}}
+            </div>
+                <span class="avatar avatar-1"><img src="/images/avatars/avatar-1.png" alt="memoji 1"></span>
                 <span class="avatar avatar-2"><img src="/images/avatars/avatar-2.png" alt="memoji 2"></span>
-                
+{{--                 
                 <span class="avatar avatar-4"><img src="/images/avatars/avatar-4.png" alt="memoji 4"></span>
                 <span class="avatar avatar-5"><img src="/images/avatars/avatar-5.png" alt="memoji 5"></span>
                 <span class="avatar avatar-6"><img src="/images/avatars/avatar-6.png" alt="memoji 6"></span>
                 <span class="avatar avatar-7"><img src="/images/avatars/avatar-7.png" alt="memoji 7"></span>
                 <span class="avatar avatar-8"><img src="/images/avatars/avatar-8.png" alt="memoji 8"></span> --}}
-            </div>
         </div>
     </div>
 </section>
