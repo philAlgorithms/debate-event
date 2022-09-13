52adc6739a649a3ac61d9bd243811207c23d873c
philAlgorithms <philippos764@gmail.com>
2022-09-13 16:34:41 +0100

    Init: schedule section

---
+++ resources/views/components/section/schedule.blade.php
@@ -0,0 +1,15 @@
+<section id="event-schedule" class="event-schedule mt-5">
+    <div class="container">
+        <div class="row text-center text-white">
+            <h1 class="display-3">Event Schedule</h1>
+	</div>
+	<div class="row d-flex justify-content-center">
+            <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+	    <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+	    <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+            <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+            <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+	    <div class="col-md-6 col-lg-4 mb-3"><x-card.event.schedule /></div>
+	</div>
+    </div> 
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
 <x-layout.main>
     <x-section.hero />
+    <x-section.schedule />
 </x-layout.main>
