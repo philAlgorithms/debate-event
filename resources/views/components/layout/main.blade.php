b2ef7b993e6219c735a975ddbe9b68b568a2643d
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 09:08:45 -0700

    Change intro font to Raleway

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="styles/css/style.css" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,700&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     {{-- <link rel="stylesheet" href="assets/css/fontawesome.css"> --}}
   </head>
   <body class="bg-dark">
