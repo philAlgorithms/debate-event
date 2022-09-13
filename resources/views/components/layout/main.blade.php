9eda5c826384ab6463e3bd77eb9a03fb5974d7a9
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 00:24:40 -0700

    Hero Intro Font size

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -7,6 +7,8 @@
 
     <title>ANYDC</title>
     <link rel="stylesheet" href="styles/css/style.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,700&display=swap" rel="stylesheet">
     {{-- <link rel="stylesheet" href="assets/css/fontawesome.css"> --}}
   </head>
   <body class="bg-dark">
--- resources/views/components/section/hero.blade.php
+++ resources/views/components/section/hero.blade.php
@@ -1,11 +1,11 @@
 <section id="hero" class="hero">
     <div class="container">
-        <div class="row text-white hero--centered">
-            <div class="col-12">
+        <div class="row text-white">
+            <div class="col-sm-12 col-md-11 col-lg-9 col-xl-10">
                 <h1 class="display-2">
                     <span class="display-2--intro">
                         Nigeria's Biggest 
-                        <span class="avatar-3-wrapper">Y<span class=""><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
+                        <span class="avatar-3-wrapper">Y<span><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
                         Gathering</span>
                     <span class="display-2--description">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
@@ -13,17 +13,25 @@
                         nesciunt labore animi blanditiis fugiat numquam pariatur.
                     </span>
                 </h1>
-                {{-- <span class="avatar-3-wrapper">Y<span><img src="/images/avatars/avatar-3.png" alt="memoji 3"></span>uth</span>
-                 --}}
             </div>
+        </div>
+        <div class="row avatars">
+            <div class="col-6 bg-primar" style="opacity: 1">
                 <span class="avatar avatar-1"><img src="/images/avatars/avatar-1.png" alt="memoji 1"></span>
-                <span class="avatar avatar-2"><img src="/images/avatars/avatar-2.png" alt="memoji 2"></span>
-{{--                 
+            </div>
+            <div class="col-6 text-center bg-success"  style="opacity: 0.1">
+                <span class="avatar avatar-2"><img src="/images/avatars/avatar-2.png" alt="memoji 2"></span>                
+            </div>
+            <div class="col-6 bg-warning"  style="opacity: 0.1">
                 <span class="avatar avatar-4"><img src="/images/avatars/avatar-4.png" alt="memoji 4"></span>
+            </div>
+            <div class="col-6 text-end bg-info"  style="opacity: 0.1">
                 <span class="avatar avatar-5"><img src="/images/avatars/avatar-5.png" alt="memoji 5"></span>
+            </div>
+            {{--
                 <span class="avatar avatar-6"><img src="/images/avatars/avatar-6.png" alt="memoji 6"></span>
-                <span class="avatar avatar-7"><img src="/images/avatars/avatar-7.png" alt="memoji 7"></span>
-                <span class="avatar avatar-8"><img src="/images/avatars/avatar-8.png" alt="memoji 8"></span> --}}
+            <span class="avatar avatar-7"><img src="/images/avatars/avatar-7.png" alt="memoji 7"></span>
+            <span class="avatar avatar-8"><img src="/images/avatars/avatar-8.png" alt="memoji 8"></span> --}}
         </div>
     </div>
 </section>
