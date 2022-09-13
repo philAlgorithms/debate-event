c90d7a1f90fd6720c7871c123281f844c557efac
philAlgorithms <philippos764@gmail.com>
2022-09-13 23:13:35 +0100

    prizes: initial set up

---
+++ resources/views/components/section/prizes.blade.php
@@ -0,0 +1,47 @@
+<section id="prizes" class="prizes mt-5">
+    <div class="container">
+	<div class="row text-center text-white">
+	    <h1 class="display-3">Prizes</h1>
+	</div>
+	<div class="row d-flex justify-content-center">
+	    <div class="col-md-6 px-1 mt-2 prize">
+		<div class="card special-guest-card">
+		    <div class="full-background" 
+			 style="background-image: url('https://i.pravatar.cc/500')"
+			 ></div>
+		    <div class="card-body pt-12">
+			<h4 class="text-white font-weight-normal"></h4>
+			<p></p>
+		    </div>
+		</div>
+	    </div>
+	    <div class="col-md-6">
+		<div class="row d-flex flex-column">
+		    <div class="col-md-7 px-1 mt-2 prize">
+			<div class="card special-guest-card h-50">
+			    <div class="full-background" 
+				 style="background-image: url('https://i.pravatar.cc/500')"
+				 ></div>
+			    <div class="card-body pt-12">
+				<h4 class="text-white font-weight-normal"></h4>
+				<p></p>
+			    </div>
+			</div>
+		    </div>
+
+		    <div class="col-md-5 px-1 mt-2 prize">
+			<div class="card special-guest-card">
+			    <div class="full-background" 
+				 style="background-image: url('https://i.pravatar.cc/500')"
+				 ></div>
+			    <div class="card-body pt-12">
+				<h4 class="text-white font-weight-normal"></h4>
+				<p></p>
+			    </div>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </div> 
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <x-layout.main>
     <x-section.hero />
     <x-section.schedule />
+    <x-section.prizes />
     <x-section.special-guests />
 </x-layout.main>
